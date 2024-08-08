7be53a89bb3d1fa0a726047b8792c5ed40b59df7
denystrypolskyi <denystrypolskyi@gmail.com>
2024-08-08 21:25:11 +0200

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { auth } from "./actions/user.actions";
@@ -12,51 +12,75 @@ import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import "./index.css";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(auth());
+    const checkIsAuthenticated = async () => {
+      try {
+        setIsLoading(true);
+        await dispatch(auth());
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    checkIsAuthenticated();
   }, []);
 
-  return (
-    <BrowserRouter>
-      <div className="bg-gray-50 min-h-screen">
-        <NavBar />
-        <Routes>
-          <Route path="/" Component={Login}></Route>
-          <Route path="/login" Component={Login}></Route>
-          <Route path="/register" Component={Register}></Route>
-
-          {/* Защищенные маршруты */}
-          <Route
-            path="/createTask"
-            element={
-              <ProtectedRoute>
-                <CreateTask />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/myTasks"
-            element={
-              <ProtectedRoute>
-                <MyTasks />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/editTask/:taskId"
-            element={
-              <ProtectedRoute>
-                <EditTask />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center bg-gray-50">
+        <LoadingSpinner />
       </div>
-    </BrowserRouter>
+    );
+  }
+
+  return (
+    <div className="bg-gray-50 min-h-screen h-screen flex flex-col">
+      {isLoading ? (
+        <div className="flex-grow flex justify-center items-center">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" Component={Login}></Route>
+            <Route path="/login" Component={Login}></Route>
+            <Route path="/register" Component={Register}></Route>
+            <Route
+              path="/createTask"
+              element={
+                <ProtectedRoute>
+                  <CreateTask />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/myTasks"
+              element={
+                <ProtectedRoute>
+                  <MyTasks />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/editTask/:taskId"
+              element={
+                <ProtectedRoute>
+                  <EditTask />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
   );
 }
 
